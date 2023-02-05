480b40bbcb03d8469f938f61cdf7611321a89b6a
Chamber6821 <vania.osinin@yandex.ru>
2023-02-05 10:16:54 +0300

    test: add VersionTests.cpp

---
+++ tests/VersionTests.cpp
@@ -0,0 +1,62 @@
+//
+// Created by Ivan on 05.02.2023.
+//
+
+#include "Version.h"
+#include <doctest.h>
+
+TEST_SUITE_BEGIN("Version comparing");
+
+TEST_CASE("first of all should compare by major version") {
+    Version left{.major = 10, .minor = 3, .patch = 1};
+    Version right{.major = 9, .minor = 6, .patch = 3};
+
+    CHECK_FALSE((left == right));
+    CHECK_FALSE((left <= right));
+    CHECK_FALSE((left < right));
+
+    CHECK((left != right));
+    CHECK((left >= right));
+    REQUIRE((left > right));
+}
+
+TEST_CASE("then should compare by minor version") {
+    Version left{.major = 2, .minor = 3, .patch = 5};
+    Version right{.major = 2, .minor = 6, .patch = 3};
+
+    CHECK_FALSE((left == right));
+    CHECK_FALSE((left >= right));
+    CHECK_FALSE((left > right));
+
+    CHECK((left != right));
+    CHECK((left <= right));
+    REQUIRE((left < right));
+}
+
+TEST_CASE("at the and should compare by patch version") {
+    Version left{.major = 2, .minor = 6, .patch = 5};
+    Version right{.major = 2, .minor = 6, .patch = 3};
+
+    CHECK_FALSE((left == right));
+    CHECK_FALSE((left <= right));
+    CHECK_FALSE((left < right));
+
+    CHECK((left != right));
+    CHECK((left >= right));
+    REQUIRE((left > right));
+}
+
+TEST_CASE("should be equals") {
+    Version left{.major = 2, .minor = 6, .patch = 3};
+    Version right{.major = 2, .minor = 6, .patch = 3};
+
+    CHECK_FALSE((left != right));
+    CHECK_FALSE((left < right));
+    CHECK_FALSE((left > right));
+
+    CHECK((left <= right));
+    CHECK((left >= right));
+    REQUIRE((left == right));
+}
+
+TEST_SUITE_END;
