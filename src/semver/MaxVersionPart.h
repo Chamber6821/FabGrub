609f9c6882d8a1dafbc2552252409f8a21045a34
Chamber6821 <vania.osinin@yandex.ru>
2023-04-19 13:58:03 +0300

    feat: add semver/Min(Max)VersionPart.h

---
+++ src/semver/MaxVersionPart.h
@@ -0,0 +1,10 @@
+//
+// Created by Ivan on 19.04.2023.
+//
+
+#pragma once
+
+#include "contract/semver/Version.h"
+#include <limits>
+
+constexpr auto MaxVersionPart = std::numeric_limits<VersionPart>::max();
---
+++ src/semver/MinVersionPart.h
@@ -0,0 +1,9 @@
+//
+// Created by Ivan on 19.04.2023.
+//
+
+#pragma once
+
+#include "contract/semver/Version.h"
+
+constexpr auto MinVersionPart = VersionPart{0};
