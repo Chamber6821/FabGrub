1e91257ff88a6f192ea294e5d2a0d51c5c88f00f
Chamber6821 <vania.osinin@yandex.ru>
2023-07-04 16:57:35 +0300

    feat: add fmt package

--- packages.cmake
+++ packages.cmake
@@ -5,3 +5,9 @@ FetchContent_Declare(
         GIT_REPOSITORY https://github.com/doctest/doctest.git
         GIT_TAG v2.4.9
 )
+
+FetchContent_Declare(
+        fmt
+        GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+        GIT_TAG 10.0.0
+)
