01c1fe9db1103a3f86c7c3b36c7725f88b3fab02
Chamber6821 <vania.osinin@yandex.ru>
2023-08-06 22:26:36 +0300

    chore: remove extra semantic typing

--- test-utils/random.cpp
+++ test-utils/random.cpp
@@ -3,32 +3,20 @@
 //
 
 #include "random.h"
-#include "semver/MinVersionPart.h"
 #include <algorithm>
+#include <utility>
 
-auto randomVersionPart() -> VersionPart {
-    const auto max = Max{VersionPart{100}};
-    return randomVersionPart(MinVersionPart, max);
-}
+auto randomVersionPart() -> int { return randomVersionPart(0, 100); }
 
-auto randomVersionPart(Min<VersionPart> min, Max<VersionPart> max)
-    -> VersionPart {
-    return random(min, max);
-}
+auto randomVersionPart(int min, int max) -> int { return random(min, max); }
 
-auto randomNoSpaceName() -> std::string {
-    const auto min = Min{3};
-    const auto max = Max{10};
-    return randomNoSpaceName(min, max);
-}
-
-auto randomNoSpaceName(Min<int> minLength, Max<int> maxLength) -> std::string {
+auto randomNoSpaceName(int minLength, int maxLength) -> std::string {
     static constexpr std::string_view alphabet = "0123456789"
                                                  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                                                  "abcdefghijklmnopqrstuvwxyz";
 
     const auto alpha = [&]() {
-        return alphabet[random(Min{std::size_t{0}}, Max{alphabet.size() - 1})];
+        return alphabet[random(std::size_t{0}, alphabet.size() - 1)];
     };
     const auto length = [&]() { return random(minLength, maxLength); };
 
--- test-utils/random.h
+++ test-utils/random.h
@@ -4,26 +4,19 @@
 
 #pragma once
 
-#include "contract/semver/Version.h"
-#include "semantic-type/Max.h"
-#include "semantic-type/Min.h"
+#include <concepts>
 #include <random>
 #include <string>
 
 template <std::integral T>
-auto random(Min<T> min, Max<T> max) -> T {
+auto random(T min, T max) -> T {
     static std::mt19937 gen(std::random_device{}());
-    std::uniform_int_distribution des{min.value(), max.value()};
+    std::uniform_int_distribution<T> des{min, max};
     return des(gen);
 }
 
-// calls randomVersionPart(min, max) with adequate values for log
-auto randomVersionPart() -> VersionPart;
+auto randomVersionPart() -> int;
 
-auto randomVersionPart(Min<VersionPart> min, Max<VersionPart> max)
-    -> VersionPart;
+auto randomVersionPart(int min, int max) -> int;
 
-// calls randomNoSpaceName(min, max) with adequate values for log
-auto randomNoSpaceName() -> std::string;
-
-auto randomNoSpaceName(Min<int> minLength, Max<int> maxLength) -> std::string;
+auto randomNoSpaceName(int minLength, int maxLength) -> std::string;
