f3569e0b3d298e6990de66fadff7bfa441c1f889
Chamber6821 <vania.osinin@yandex.ru>
2023-08-08 21:41:38 +0300

    feat(App.h): add constructor and method

--- src/App.h
+++ src/App.h
@@ -4,13 +4,22 @@
 
 #pragma once
 
+#include <utility>
+
+#include "destination/Destination.h"
 #include "profile/Profiles.h"
 #include "repository/Repository.h"
-#include "destination/Destination.h"
 
 class App {
+    ptr<Profiles> profiles;
+    ptr<Repository> repository;
+    ptr<Destination> destination;
+
   public:
-    App() {}
+    App(ptr<Profiles> profiles, ptr<Repository> repository,
+        ptr<Destination> destination)
+        : profiles(std::move(profiles)), repository(std::move(repository)),
+          destination(std::move(destination)) {}
 
     void load(std::string profile) {}
 };
