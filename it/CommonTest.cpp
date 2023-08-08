aa30b3c45537b5dcb5253f3a3413b2d514cccab4
Chamber6821 <vania.osinin@yandex.ru>
2023-08-08 21:30:44 +0300

    test(it/CommonTest.cpp): update test with new features

--- it/CommonTest.cpp
+++ it/CommonTest.cpp
@@ -2,7 +2,16 @@
 // Created by Ivan on 05.08.2023.
 //
 
+#include "App.h"
+#include "destination/MemDestination.h"
 #include "doctest-nolint.h"
+#include "package/MemPackage.h"
+#include "package/MemPackages.h"
+#include "profile/MemProfile.h"
+#include "profile/MemProfiles.h"
+#include "repository/MemRepository.h"
+#include "requirement/MemRequirement.h"
+#include "requirement/MemRequirements.h"
 
 TEST_SUITE("Common integration test") {
     TEST_CASE("") {
@@ -11,22 +20,26 @@ TEST_SUITE("Common integration test") {
         make<App>(
             make<MemProfiles>(make<MemProfile>(
                 "profile",
-                make<Requirements>(
-                    make<Requirement>("a", make<Interval>("1.0.0", "2.0.0"))
+                make<MemRequirements>(
+                    make<MemRequirement>("a", "1.0.0", "2.0.0")
                 )
             )),
-            make<MemRepository>(
-                make<MemPackage>("a", "1.0.0", {}, "bytes-for-1.0.0"),
-                make<MemPackage>("a", "1.0.1", {}, "bytes-for-1.0.1"),
-                make<MemPackage>("a", "1.0.2", {}, "bytes-for-1.0.2"),
-                make<MemPackage>("a", "1.1.0", {}, "bytes-for-1.1.0"),
-                make<MemPackage>("a", "2.0.0", {}, "bytes-for-2.0.0")
-            ),
+            make<MemRepository>(std::pair{
+                "a",
+                make<MemPackages>(
+                    make<MemPackage>("a", "1.0.0", "bytes-for-1.0.0"),
+                    make<MemPackage>("a", "1.0.1", "bytes-for-1.0.1"),
+                    make<MemPackage>("a", "1.0.2", "bytes-for-1.0.2"),
+                    make<MemPackage>("a", "1.1.0", "bytes-for-1.1.0"),
+                    make<MemPackage>("a", "2.0.0", "bytes-for-2.0.0")
+                )}),
             destination
         )
             ->load("profile");
 
         CHECK(destination->count() == 1);
-        CHECK(destination->containsWithVersion("a", "1.1.0"));
-}
+        auto package = destination->at(0);
+        CHECK(package->name() == "a");
+        CHECK(*package->version() == VersionOf("1.1.0"));
+    }
 }
