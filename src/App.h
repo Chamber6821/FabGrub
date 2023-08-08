f365036c263b564a70a49cff58d29862f994004d
Chamber6821 <vania.osinin@yandex.ru>
2023-08-08 21:29:47 +0300

    feat(App.h): add draft of App

---
+++ src/App.h
@@ -0,0 +1,16 @@
+//
+// Created by Ivan on 08.08.2023.
+//
+
+#pragma once
+
+#include "profile/Profiles.h"
+#include "repository/Repository.h"
+#include "destination/Destination.h"
+
+class App {
+  public:
+    App() {}
+
+    void load(std::string profile) {}
+};
