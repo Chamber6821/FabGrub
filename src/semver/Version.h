25ead7f43e2a128bb88a40120666ad36524c850d
Chamber6821 <vania.osinin@yandex.ru>
2023-08-08 21:27:29 +0300

    feat(semver/Version.h): add comparators for Version

--- src/semver/Version.h
+++ src/semver/Version.h
@@ -5,10 +5,21 @@
 #pragma once
 
 #include "Reactions/misc/Interface.h"
+#include <tuple>
 
 class Version : public Interface {
   public:
     virtual auto major() -> int = 0;
     virtual auto minor() -> int = 0;
     virtual auto patch() -> int = 0;
+
+    auto operator==(Version &other) {
+        return std::make_tuple(major(), minor(), patch()) ==
+               std::make_tuple(other.major(), other.minor(), other.patch());
+    }
+
+    auto operator<=>(Version &other) {
+        return std::make_tuple(major(), minor(), patch()) <=>
+               std::make_tuple(other.major(), other.minor(), other.patch());
+    }
 };
