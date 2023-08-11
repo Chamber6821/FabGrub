990f69f90bfc2c064a656418060d40fe14812cec
Chamber6821 <vania.osinin@yandex.ru>
2023-08-11 12:34:23 +0300

    feat(semver/Version.h): add output operator overloading

--- src/semver/Version.h
+++ src/semver/Version.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "Reactions/misc/Interface.h"
+#include <ostream>
 #include <tuple>
 
 class Version : public Interface {
@@ -21,5 +22,10 @@ class Version : public Interface {
     auto operator<=>(const Version &other) const {
         return std::make_tuple(major(), minor(), patch()) <=>
                std::make_tuple(other.major(), other.minor(), other.patch());
+    }
+
+    friend auto operator<<(std::ostream &out, const Version &v)
+        -> std::ostream & {
+        return out << v.major() << '.' << v.minor() << '.' << v.patch();
     }
 };
