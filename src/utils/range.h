0ca6dd64a1632ffa05c9e916a66d98e6d9cf8709
Chamber6821 <vania.osinin@yandex.ru>
2023-08-12 15:04:55 +0300

    feat(utild/range.h): add tool to get constructor of class

--- src/utils/range.h
+++ src/utils/range.h
@@ -13,3 +13,16 @@ auto to_range(ptr<List<T>> list) {
     return std::views::iota(0, list->count()) |
            std::views::transform([list](int index) { return list->at(index); });
 }
+
+namespace details {
+template <class T>
+struct constructor_of {
+    template <class... Args>
+    constexpr auto operator()(Args &&...args) const -> T {
+        return T(std::forward<Args>(args)...);
+    }
+};
+} // namespace details
+
+template <class T>
+constexpr details::constructor_of<T> constructor;
