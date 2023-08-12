6a61a0fc9077684eacbd4896f0130b04da790dff
Chamber6821 <vania.osinin@yandex.ru>
2023-08-12 15:04:07 +0300

    feat(utils/MemList.h): add constructor from range

--- src/utils/MemList.h
+++ src/utils/MemList.h
@@ -5,6 +5,8 @@
 #pragma once
 
 #include "List.h"
+#include <concepts>
+#include <ranges>
 #include <vector>
 
 template <class T>
@@ -18,6 +20,12 @@ class MemList : public List<T> {
 
     template <std::convertible_to<T>... Args>
     explicit MemList(Args... args) : MemList({args...}) {}
+
+    template <std::input_iterator It>
+    MemList(It begin, It end) : MemList(std::vector<T>(begin, end)) {}
+
+    template <std::ranges::input_range Range>
+    explicit MemList(Range range) : MemList(range.begin(), range.end()) {}
 
     [[nodiscard]] auto count() const -> int override {
         return static_cast<int>(reqs.size());
