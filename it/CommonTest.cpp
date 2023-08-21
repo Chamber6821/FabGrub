d40ab3f2c7d731b0cdf6ba0ce2718f603e19980e
Chamber6821 <vania.osinin@yandex.ru>
2023-08-21 12:41:05 +0300

    test(it/ComminTest.cpp): update for new features

--- it/CommonTest.cpp
+++ it/CommonTest.cpp
@@ -4,36 +4,63 @@
 
 #include "destination/MemDestination.h"
 #include "doctest-nolint.h"
+#include "http/HttpClient.h"
+#include "http/LoggedHttp.h"
+#include "log/ForkedLog.h"
+#include "log/StreamLog.h"
 #include "package/MemPackage.h"
 #include "repository/MemRepository.h"
+#include "repository/re146/Repository.h"
 #include "requirement/MemRequirement.h"
 #include "solution/PubgrubSolution.h"
+
+auto operator<<(std::ostream &out, const Package &p) -> std::ostream & {
+    return out << fmt::format("{}:{}", p.name(), fmt::streamed(*p.version()));
+}
+
+// NOLINTNEXTLINE(*-no-recursion)
+auto stringify(const std::exception &e, int level) -> std::string {
+    auto part = std::string(level, ' ') + "exception: " + e.what() + '\n';
+    try {
+        std::rethrow_if_nested(e);
+        return part;
+    } catch (const std::exception &nestedException) {
+        return part + stringify(nestedException, level + 1);
+    }
+}
 
 TEST_SUITE("Common integration test") {
     TEST_CASE("") {
         auto destination = make<MemDestination>();
-
-        destination->fill(
-            make<PubgrubSolution>(
-                make<MemRequirements>(
-                    make<MemRequirement>("a", "1.0.0", "2.0.0")
-                ),
-                make<MemRepository>(std::pair{
-                    "a",
-                    make<MemPackages>(
-                        make<MemPackage>("a", "1.0.0", "bytes-for-1.0.0"),
-                        make<MemPackage>("a", "1.0.1", "bytes-for-1.0.1"),
-                        make<MemPackage>("a", "1.0.2", "bytes-for-1.0.2"),
-                        make<MemPackage>("a", "1.1.0", "bytes-for-1.1.0"),
-                        make<MemPackage>("a", "2.0.0", "bytes-for-2.0.0")
-                    )})
-            )
-                ->packages()
+        auto log = make<ForkedLog>(
+            make<StreamLog>(std::shared_ptr<std::ostream>(
+                std::shared_ptr<nullptr_t>(),
+                &std::cout
+            )),
+            make<StreamLog>(make<std::ofstream>("log.txt"))
         );
 
-        CHECK(destination->count() == 1);
-        auto package = destination->at(0);
-        CHECK(package->name() == "a");
-        CHECK(*package->version() == VersionOf("1.1.0"));
+        try {
+            destination->fill(
+                make<PubgrubSolution>(
+                    make<MemRequirements>(make<MemRequirement>(
+                        "space-exploration",
+                        "1.0.0",
+                        "2.0.0"
+                    )),
+                    make<MemCachedRepository>(make<Re146Repository>(
+                        make<LoggedHttp>(make<HttpClient>(), log)
+                    ))
+                )
+                    ->packages()
+            );
+
+            for (const auto &p : to_range<ptr<Package>>(destination)) {
+                std::cout << *p << '\n';
+            }
+        } catch (const std::exception &e) {
+            std::cout << stringify(e, 0) << '\n';
+            throw e;
+        }
     }
 }
