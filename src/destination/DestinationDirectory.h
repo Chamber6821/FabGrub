4e865a8a07548d178121d6a94298dda793274b4f
Chamber6821 <vania.osinin@yandex.ru>
2023-08-28 17:46:37 +0300

    feat(destination/DestinationDirectory.h): use FileRepository instead of Package::file()

--- src/destination/DestinationDirectory.h
+++ src/destination/DestinationDirectory.h
@@ -5,22 +5,26 @@
 #pragma once
 
 #include "Destination.h"
+#include "file-repository/FileRepository.h"
 #include "fmt/format.h"
 #include "fmt/ostream.h"
 #include "utils/range.h"
 
 class DestinationDirectory : public Destination {
     std::filesystem::path dir;
+    ptr<FileRepository> repo;
 
   public:
-    explicit DestinationDirectory(std::filesystem::path dir)
-        : dir(std::move(dir)) {}
+    explicit DestinationDirectory(
+        std::filesystem::path dir, ptr<FileRepository> repo
+    )
+        : dir(std::move(dir)), repo(std::move(repo)) {}
 
     void fill(ptr<Packages> filling) override {
         try {
             std::filesystem::create_directories(dir);
             for (auto p : to_range(filling)) {
-                p->file()->saveTo(dir);
+                repo->fileFor(p)->saveTo(dir);
             }
         } catch (...) {
             std::throw_with_nested(std::runtime_error(fmt::format(
