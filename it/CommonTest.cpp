ac7bb52b369309be3153ed1c65bc1219aaeab978
Chamber6821 <vania.osinin@yandex.ru>
2023-08-29 19:00:40 +0300

    refactor(it/CommonTest.cpp): move app creation to outside try-catch

--- it/CommonTest.cpp
+++ it/CommonTest.cpp
@@ -2,6 +2,7 @@
 // Created by Ivan on 05.08.2023.
 //
 
+#include "action/SequentialFilling.h"
 #include "destination/DestinationDirectory.h"
 #include "destination/LoggedDestination.h"
 #include "doctest-nolint.h"
@@ -42,43 +43,41 @@ TEST_SUITE("Common integration test") {
         );
         auto http = make<LoggedHttp>(make<HttpClient>(), log);
         auto basePackage = make<MemPackage>("base", "1.1.80");
-
-        // fixes text of std::filesystem::filesystem_error
-        // NOLINTNEXTLINE(*-mt-unsafe)
-        (void)std::setlocale(LC_ALL, "");
-        // NOLINTNEXTLINE(*-mt-unsafe)
-        log->info("Current locale: {}", std::setlocale(LC_ALL, nullptr));
-
-        try {
-            make<SequencialFilling>(
-                make<LoggedDestination>(
-                    make<DestinationDirectory>(
-                        "mods",
-                        make<OverloadedFileRepository>(
-                            basePackage,
-                            make<FakeFile>(),
-                            make<FileCachedFileRepository>(
-                                "mods-cached",
-                                make<re146::FileRepository>(http)
-                            )
+        auto app = make<SequentialFilling>(
+            make<LoggedDestination>(
+                make<DestinationDirectory>(
+                    "mods",
+                    make<OverloadedFileRepository>(
+                        basePackage,
+                        make<FakeFile>(),
+                        make<FileCachedFileRepository>(
+                            "mods-cached",
+                            make<re146::FileRepository>(http)
                         )
-                    ),
-                    log
-                ),
-                make<PubgrubSolution>(
-                    make<MemRequirements>(make<MemRequirement>(
-                        "space-exploration",
-                        "0.0.0",
-                        "1.0.0"
-                    )),
-                    make<OverloadedRepository>(
-                        make<re146::Repository>(make<MemCachedHttp>(http)),
-                        "base",
-                        make<MemPackages>(basePackage)
                     )
+                ),
+                log
+            ),
+            make<PubgrubSolution>(
+                make<MemRequirements>(
+                    make<MemRequirement>("space-exploration", "0.0.0", "1.0.0")
+                ),
+                make<OverloadedRepository>(
+                    make<re146::Repository>(make<MemCachedHttp>(http)),
+                    "base",
+                    make<MemPackages>(basePackage)
                 )
             )
-                ->fill();
+        );
+
+        try {
+            // fixes text of std::filesystem::filesystem_error
+            // NOLINTNEXTLINE(*-mt-unsafe)
+            (void)std::setlocale(LC_ALL, "");
+            // NOLINTNEXTLINE(*-mt-unsafe)
+            log->info("Current locale: {}", std::setlocale(LC_ALL, nullptr));
+
+            (*app)();
         } catch (const std::exception &e) {
             log->info("{}", stringify(e, 0));
             throw e;
