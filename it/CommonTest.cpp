276b9f8f922473e2fef30c5f2c2fbda0038b6197
Chamber6821 <vania.osinin@yandex.ru>
2023-08-31 10:03:31 +0300

    test(it/CommonTest.cpp): update for new features

--- it/CommonTest.cpp
+++ it/CommonTest.cpp
@@ -36,6 +36,8 @@ auto stringify(const std::exception &e, int level) -> std::string {
 
 TEST_SUITE("Common integration test") {
     TEST_CASE("") {
+        auto profileName = "test";
+
         auto log = make<SynchronizedLog>(make<ForkedLog>(
             make<StreamLog>(std::shared_ptr<std::ostream>(
                 std::shared_ptr<std::nullptr_t>(),
@@ -44,13 +46,8 @@ TEST_SUITE("Common integration test") {
             make<StreamLog>(make<std::ofstream>("log.txt"))
         ));
         auto http = make<LoggedHttp>(log, make<HttpClient>());
-        auto profile = make<MemProfile>(
-            "test",
-            make<VersionOf>("1.1.80"),
-            make<MemRequirements>(
-                make<FactorioRequirement>("space-exploration >= 0.0.0")
-            )
-        );
+        auto profile =
+            make<FileProfile>(fmt::format("profiles/{}.json", profileName));
         auto basePackage = make<MemPackage>("base", profile->factorioVersion());
         auto app = make<SequentialFilling>(
             make<LoggedDestination>(
@@ -67,12 +64,15 @@ TEST_SUITE("Common integration test") {
                     )
                 )
             ),
-            make<PubgrubSolution>(
-                profile->requirements(),
-                make<OverloadedRepository>(
-                    basePackage->name(),
-                    make<MemPackages>(basePackage),
-                    make<re146::Repository>(make<MemCachedHttp>(http))
+            make<FileCachedSolution>(
+                fmt::format("profiles/{}.json.lock", profileName),
+                make<PubgrubSolution>(
+                    profile->requirements(),
+                    make<OverloadedRepository>(
+                        basePackage->name(),
+                        make<MemPackages>(basePackage),
+                        make<re146::Repository>(make<MemCachedHttp>(http))
+                    )
                 )
             )
         );
@@ -83,7 +83,7 @@ TEST_SUITE("Common integration test") {
             (void)std::setlocale(LC_ALL, "");
             // NOLINTNEXTLINE(*-mt-unsafe)
             log->info("Current locale: {}", std::setlocale(LC_ALL, nullptr));
-            log->info("Chosen profile: {}", profile->name());
+            log->info("Chosen profile: {}", profileName);
 
             (*app)();
         } catch (const std::exception &e) {
