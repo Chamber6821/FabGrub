d2584b44dbc88692d15945474338705903f55e90
Chamber6821 <vania.osinin@yandex.ru>
2023-09-04 18:35:36 +0300

    feat(action/Protectedpath.h): add move logging

--- src/action/ProtectedPath.h
+++ src/action/ProtectedPath.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include "Action.h"
+#include "Reactions/misc/owning/make.h"
 #include "fmt/format.h"
 #include "fmt/ostream.h"
+#include "log/FakeLog.h"
+#include "log/Log.h"
 #include "utils/defer.h"
 #include "utils/ptr.h"
 #include <filesystem>
@@ -11,22 +14,35 @@
 class ProtectedPath : public Action {
     std::filesystem::path target;
     std::filesystem::path savePath;
+    ptr<Log> log;
     ptr<Action> origin;
 
   public:
     ProtectedPath(
         std::filesystem::path target, std::filesystem::path savePath,
-        ptr<Action> origin
+        ptr<Log> log, ptr<Action> origin
     )
         : target(std::move(target)), savePath(std::move(savePath)),
-          origin(std::move(origin)) {}
+          log(std::move(log)), origin(std::move(origin)) {}
+
+    ProtectedPath(
+        std::filesystem::path target, std::filesystem::path savePath,
+        ptr<Action> origin
+    )
+        : ProtectedPath(target, savePath, make<FakeLog>(), origin) {}
 
     void operator()() override {
         try {
             std::filesystem::create_directories(savePath);
             std::filesystem::remove_all(savePath);
-            if (std::filesystem::exists(target))
+            if (std::filesystem::exists(target)) {
+                log->info(
+                    "Move {} to {}",
+                    fmt::streamed(target),
+                    fmt::streamed(savePath)
+                );
                 std::filesystem::rename(target, savePath);
+            }
         } catch (...) {
             std::throw_with_nested(std::runtime_error(fmt::format(
                 "Failed while move {} to {}",
@@ -38,8 +54,14 @@ class ProtectedPath : public Action {
         const defer finally([&] {
             try {
                 std::filesystem::remove_all(target);
-                if (std::filesystem::exists(savePath))
+                if (std::filesystem::exists(savePath)) {
+                    log->info(
+                        "Move {} to {}",
+                        fmt::streamed(savePath),
+                        fmt::streamed(target)
+                    );
                     std::filesystem::rename(savePath, target);
+                }
             } catch (...) {
                 std::throw_with_nested(std::runtime_error(fmt::format(
                     "Failed while restore {} from {}",
