f3e831db33b916bfe4d34cadcfcfcc4e30fa64b5
Chamber6821 <vania.osinin@yandex.ru>
2023-09-24 18:10:05 +0300

    refactor(utils/urlEncoded): change to appease clang-tidy

--- src/utils/urlEncoded.cpp
+++ src/utils/urlEncoded.cpp
@@ -3,11 +3,11 @@
 #include "defer.h"
 
 auto urlEncoded(const std::string &urlPart) -> std::string {
-    const auto encoded_value = curl_easy_escape(
+    char *encoded_value = curl_easy_escape(
         nullptr,
         urlPart.c_str(),
         static_cast<int>(urlPart.length())
     );
-    defer finally([&] { curl_free(encoded_value); });
+    const defer finally([&] { curl_free(encoded_value); });
     return encoded_value;
 }
