3a6b1b48d60614a4febb9ee433c49426f06dfa9a
Chamber6821 <vania.osinin@yandex.ru>
2023-09-24 19:05:00 +0300

    feat(requirement/FactorioReqirement): understand partial syntax. fixes #8

--- src/requirement/FactorioReqirement.h
+++ src/requirement/FactorioReqirement.h
@@ -13,7 +13,8 @@
 
 class FactorioRequirement : public Requirement {
     std::string line;
-    std::regex regex{R"(^~?\s*(.+?)\s*(\S+)\s*(\S+)$)"};
+    std::regex full{R"(^~?\s*(.+?)\s*([<=>]+)\s*([0-9.]+)$)"};
+    std::regex partial{R"(^~?\s*(.+?)$)"};
 
   public:
     explicit FactorioRequirement(std::string line) : line(std::move(line)) {}
@@ -21,7 +22,8 @@ class FactorioRequirement : public Requirement {
     [[nodiscard]] auto name() const -> std::string override {
         try {
             std::smatch match;
-            std::regex_match(line, match, regex);
+            std::regex_match(line, match, full);
+            if (match.empty()) std::regex_match(line, match, partial);
             return match[1];
         } catch (...) {
             std::throw_with_nested(std::runtime_error(
@@ -33,7 +35,8 @@ class FactorioRequirement : public Requirement {
     [[nodiscard]] auto low() const -> ptr<Version> override {
         try {
             std::smatch match;
-            std::regex_match(line, match, regex);
+            std::regex_match(line, match, full);
+            if (match.empty()) return make<VersionOf>("0.0.0");
 
             if (match[2] == "<") return make<VersionOf>("0.0.0");
             if (match[2] == "<=") return make<VersionOf>("0.0.0");
@@ -55,17 +58,19 @@ class FactorioRequirement : public Requirement {
 
     [[nodiscard]] auto high() const -> ptr<Version> override {
         try {
+            const auto maxVersion = std::string("999999.999999.999999");
+
             std::smatch match;
-            std::regex_match(line, match, regex);
+            std::regex_match(line, match, full);
+            if (match.empty()) return make<VersionOf>(maxVersion);
 
             if (match[2] == "<") return make<FactorioVersionOf>(match[3]);
             if (match[2] == "<=")
                 return make<NextPatch>(make<FactorioVersionOf>(match[3]));
             if (match[2] == "=")
                 return make<NextPatch>(make<FactorioVersionOf>(match[3]));
-            if (match[2] == ">=")
-                return make<VersionOf>("999999.999999.999999");
-            if (match[2] == ">") return make<VersionOf>("999999.999999.999999");
+            if (match[2] == ">=") return make<VersionOf>(maxVersion);
+            if (match[2] == ">") return make<VersionOf>(maxVersion);
 
             throw std::runtime_error(fmt::format(
                 "Invalid equality operator in requirement line \"{}\"",
