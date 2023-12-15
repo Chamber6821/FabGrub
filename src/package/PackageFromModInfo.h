4552d4ab989fbb374525e4067020e7543dafed6a
Chamber6821 <vania.osinin@yandex.ru>
2023-12-15 22:46:41 +0300

    feat(package): add PackageFromModInfo. #16

---
+++ src/package/PackageFromModInfo.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Package.h"
+#include "Reactions/misc/Scalar.h"
+#include "Reactions/misc/owning/make.h"
+#include "nlohmann/json.hpp"
+#include "re146/JsonReqirements.h"
+#include "scalar/JsonFromFile.h"
+#include "semver/VersionOf.h"
+
+class PackageFromModInfo : public Package {
+    ptr<Scalar<nlohmann::json>> json;
+
+  public:
+    explicit PackageFromModInfo(ptr<Scalar<nlohmann::json>> json)
+        : json(std::move(json)) {}
+
+    explicit PackageFromModInfo(std::filesystem::path file)
+        : PackageFromModInfo(make<JsonFromFile>(std::move(file))) {}
+
+    [[nodiscard]] auto name() const -> std::string override {
+        return json->value()["name"].get<std::string>();
+    }
+
+    [[nodiscard]] auto version() const -> ptr<Version> override {
+        return make<VersionOf>(json->value()["version"].get<std::string>());
+    }
+
+    [[nodiscard]] auto requirements() const -> ptr<Requirements> override {
+        return make<re146::JsonRequirements>(json->value()["dependencies"]);
+    }
+};
