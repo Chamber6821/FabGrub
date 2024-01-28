848d2c6224c248c756487ee57673fa9b30d59bc8
Chamber6821 <vania.osinin@yandex.ru>
2024-01-29 01:40:59 +0300

    fix(test-utils): add const qualifier for distribution

--- test-utils/random.h
+++ test-utils/random.h
@@ -11,7 +11,7 @@
 template <std::integral T>
 auto random(T min, T max) -> T {
     static std::mt19937 gen(std::random_device{}());
-    std::uniform_int_distribution<T> des{min, max};
+    const std::uniform_int_distribution<T> des{min, max};
     return des(gen);
 }
 
